e596096bb2a9803b59747ba852dbfe0171480a73
kev1nandreas <kevin.andreascn@gmail.com>
2024-10-18 15:16:35 +0700

    Refactor login functionality and views

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request){
+
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Routing\Route as RoutingRoute;
 
 Route::get('/', function () {
+    if (auth()->check() == false) {
+        return redirect('/login');
+    }
+
     return view('landingpage');
 });
 
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/login', [LoginController::class, 'authenticate']);
